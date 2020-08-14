1e7aaad02132f1a955265b73cf4e490b7b9af17d
Daniel Sneijers <daniel.sneijers@gmail.com>
2020-08-14 14:06:39 +0200

    updated all dependencies to latest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  setupTestFrameworkScriptFile: '<rootDir>/jest.setup.js',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleDirectories: ['node_modules'],
   moduleFileExtensions: ['js', 'jsx'],
   coveragePathIgnorePatterns: ['<rootDir>/jest.setup.js', '<rootDir>/node_modules/'],
+  snapshotSerializers: ['enzyme-to-json/serializer'],
 };
